a68a7f58b934157b15dba0cb67dbb9964492d8c6
pauldbd <loldeadmemegod@gmail.com>
2025-01-05 19:06:30 -0600

    Added preprocessing to turn data into tensors

--- dataset.py
+++ dataset.py
@@ -5,18 +5,39 @@ from torch.utils.data import Dataset
 
 
 class GraphDataset(Dataset):
-    def __init__(self, df):
+    def __init__(self, df, device = None):
         self.df = df
         self.molecule_graphs = pickle.load(open("data/graphs/molecule_graph.pkl", "rb"))
         self.protein_graphs = pickle.load(open("data/graphs/protein_graph.pkl", "rb"))
+        self.device = "mps"
+        if (device):
+            self.device = device
+            
 
     def __len__(self):
         return len(self.df)
+    
+    def toTensor(self, val):
+        return torch.Tensor(val, dtype=torch.float).to(self.device)
 
     def __getitem__(self, idx):
         #molecule_smiles,protein_name,binds
         molecule_smiles = self.df['molecule_smiles'][idx]
         protein_name = self.df['protein_name'][idx]
-        binds = self.df['binds'][idx]
-        return self.molecule_graphs.get(molecule_smiles), self.protein_graphs.get(protein_name), binds
+        binds = torch.Tensor(self.df['binds'][idx]).to(self.device)
+        
+        mg = self.molecule_graphs.get(molecule_smiles)
+        pg = self.protein_graphs.get(protein_name)
+
+        mg.x = self.toTensor(mg.x)
+        pg.x = self.toTensor(pg.x)
+
+        mg.edge_attr = self.toTensor(mg.edge_attr)
+        pg.edge_attr = self.toTensor(pg.edge_attr)
+
+        mg.edge_index = self.toTensor(mg.edge_index)
+        pg.edge_index = self.toTensor(pg.edge_index)
+
+
+        return mg, pg, binds
     
