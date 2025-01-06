3ab59b557dc2d147095f476a5d0bd8df7cfe8e31
pauldbd <loldeadmemegod@gmail.com>
2025-01-06 01:56:49 -0600

    Fixed minor bugs

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from .dataset import GraphDataset
+from dataset import GraphDataset
 import pandas as pd
 from model import GraphNN
 import torch
@@ -8,34 +8,36 @@ import sys
 
 def train(model: torch.nn.Module, dataloader: DataLoader, criterion: torch.nn.Module, optimizer: torch.optim.Optimizer, tracker = None):
     model.train()
-    for i, data in dataloader: 
+    for i, data in enumerate(dataloader): 
 
         optimizer.zero_grad()
 
         mg, pg, bind = data
         
-        out = model(mg, pg)
+        out = model(mg, pg).to("mps")
         loss = criterion(out, bind)
 
         loss.backward()
         optimizer.step()
 
-        tracker(**locals())
+        tracker("train", locals())
 
     return model
 
 
 if __name__ == "__main__":
-    train_df = pd.read_csv("data/normalized_training.csv")
+    train_df = pd.read_csv("data/normalized_train.csv")
     test_df = pd.read_csv("data/normalized_test.csv")
     
     train_dataset = GraphDataset(train_df)
     test_dataset = GraphDataset(test_df)
 
+    print(train_dataset[0][-1])
+
     batch_size = 20
 
-    training_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+    training_dataloader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=GraphDataset.collate)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=GraphDataset.collate)
 
     mg_samp, pg_samp, _ = train_dataset[0]
 
@@ -47,7 +49,10 @@ if __name__ == "__main__":
         tracker_path = sys.argv[1]
         tracker = kujira.init(tracker_path)
 
-    train(training_dataloader, tracker)
+    criterion = torch.nn.BCELoss().to("mps")
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+
+    train(model, training_dataloader, criterion, optimizer, tracker)
 
 
 
