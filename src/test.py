6fc26f3e0a18cc97bd9468690634bde97fdd760f
pauldbd <loldeadmemegod@gmail.com>
2025-01-09 18:39:48 -0600

    Created the testing framework and made testing go in chunks

--- src/test.py
+++ src/test.py
@@ -1,3 +1,6 @@
+import os
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
 from data_prep import graph_data
 import pandas as pd
 from model import GraphNN
@@ -16,12 +19,16 @@ output_file = "results/test/output.csv"
 
 model = None
 
+# os.environ["PYTORCH_ENABLE_MPS_FALLBACK"]="1"
+
 for test_df in tqdm(pd.read_csv(test_file, chunksize=100000)):
+
+    test_df.reset_index(drop=True, inplace=True)
 
     protein_graphs, molecule_graphs = graph_data(dataframe=test_df).pack()
 
     test_dataset = GraphDataset(test_df, training=False, molecule_graphs=molecule_graphs, protein_graphs=protein_graphs)
-    batch_size = 40
+    batch_size = 300
 
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=GraphDataset.collate_test)
 
@@ -30,6 +37,7 @@ for test_df in tqdm(pd.read_csv(test_file, chunksize=100000)):
     if (model == None):
         model = GraphNN(out_channels=32, heads=4, num_layers=10, edge_feature_dims=(mg_samp.edge_attr.shape[-1], pg_samp.edge_attr.shape[-1]))
         model.load_state_dict(torch.load("models/gnn-01-06.zip", weights_only=False))
+        model = model.to("mps")
 
     res = pred(model, test_dataloader)
     
